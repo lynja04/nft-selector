1b3e98c2440783d82ee791bc2b817d6366bfd690
James Lynn <j.lynn3237@gmail.com>
2022-03-20 14:31:59 -0400

    initial commit

---
+++ src/main/java/com/jimmylynn/NftSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.jimmylynn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftSelectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimmylynn/NftSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmylynn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
